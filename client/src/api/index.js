893c7c385559a3dbfcde5539df522114a982fda2
刘帅彤 <liushuaitong@Venessa.cable.rcn.com>
2020-08-16 02:50:45 -0400

    baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://film4seasons.herokuapp.com/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
